236d490e41394232c11c18d955c904f856499047
SumandoAritonang <aritonang720@gmail.com>
2021-10-19 15:32:12 +0700

    -

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+   {
+    echo "<h1>Halo</h1>";
+    echo"Nama Sumando Aritonang <br>
+        Hobiku Bermain Catur <br>
+        Tempat lahir Bogor
+        Alamat Cilebut barat
+        hobi Bermain catur
+        ";
+
+   }
+}
